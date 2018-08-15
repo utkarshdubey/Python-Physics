b5b425149cfe36ce42722a9b50ed69f35415d5ca
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2018-08-15 18:40:19 +0530

    Added another feature.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # imports
-from functions.functions import DistanceFinderOne, FinalVelocityFinderOne
+from functions.functions import DistanceFinderOne, DistanceFinderTwo, FinalVelocityFinderOne
 import sys
 # logic here
 
 while True:
     print("""
     1 => Find Distance, and I have U, A and T
-    2 => Find Final Velocity, and I have U, A and T
+    2 => Find Distance, and I have U, V and A
+    3 => Find Final Velocity, and I have U, A and T
+
     0 => Exit
     """)
     answer = int(input("Please enter one of the options: "))
@@ -18,9 +20,17 @@ while True:
 
     if answer == 2:
         u_2 = float(input("Please enter U: "))
+        v_2 = float(input("Please enter V: "))
         a_2 = float(input("Please enter A: "))
-        t_2 = float(input("Please enter T: "))
+
+        DistanceFinderTwo(u_2, v_2, a_2)
+
+    if answer == 3:
+        u_3 = float(input("Please enter U: "))
+        a_3 = float(input("Please enter A: "))
+        t_3 = float(input("Please enter T: "))
         FinalVelocityFinderOne(u_2, a_2, t_2)
+
 
     if answer == 0:
         sys.exit()
