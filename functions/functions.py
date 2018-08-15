b95c70d7699e8571f6307d0fcb956023cbd6c93f
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2018-08-15 20:45:50 +0530

    Finished a lot of things.

--- functions/functions.py
+++ functions/functions.py
@@ -10,3 +10,20 @@ def DistanceFinderTwo(u, v, a):
 def FinalVelocityFinderOne(u, a, t):
     final_velocity = float(u + a * t)
     print("The final velcoity is " + str(final_velocity) + " m/s")
+
+def FinalVelocityFinderTwo(u, a, s):
+    x = 2 * a * s
+    final_velocity = float(pow(x, 1/2) + u)
+    print("The final velcoity is " + str(final_velocity) + " m/s")
+
+def TimeFinderOne(u, v, a):
+    time = float(v / (u + a))
+    print("The time is " + str(time) + " seconds")
+
+def AccelarationFinderOne(u, v, t):
+    accelaration = float((v - u) / t)
+    print("The accelaration is " + str(accelaration) + " m/s^2")
+
+def UniformVelocityFinderOne(v, a, t):
+    uniform_velocity = float(v + a * t)
+    print("The uniform velocity is " + str(uniform_velocity) + " m/s")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # imports
-from functions.functions import DistanceFinderOne, DistanceFinderTwo, FinalVelocityFinderOne
+from functions.functions import DistanceFinderOne, DistanceFinderTwo, FinalVelocityFinderOne, FinalVelocityFinderTwo, TimeFinderOne, AccelarationFinderOne, UniformVelocityFinderOne
 import sys
 # logic here
 
@@ -8,6 +8,10 @@ while True:
     1 => Find Distance, and I have U, A and T
     2 => Find Distance, and I have U, V and A
     3 => Find Final Velocity, and I have U, A and T
+    4 => Find Final Velocity, and I have U, A and S
+    5 => Find Accelaration, and I have U, V and T
+    6 => Find Time, and I have U, V and A
+    7 => Find Uniform Accelaration, and I have V, A and T
 
     0 => Exit
     """)
@@ -29,10 +33,37 @@ while True:
         u_3 = float(input("Please enter U: "))
         a_3 = float(input("Please enter A: "))
         t_3 = float(input("Please enter T: "))
-        FinalVelocityFinderOne(u_2, a_2, t_2)
+
+        FinalVelocityFinderOne(u_3, a_3, t_3)
+
+    if answer == 4:
+        u_6 = float(input("Please enter U: "))
+        a_6 = float(input("Please enter A: "))
+        s_6 = float(input("Please enter S: "))
+
+        FinalVelocityFinderTwo(u_6, a_6, s_6)
+
+    if answer == 5:
+        u_5 = float(input("Please enter U: "))
+        v_5 = float(input("Please enter V: "))
+        t_5 = float(input("Please enter T: "))
+
+        AccelarationFinderOne(u_5, v_5, t_5)
+
+    if answer == 6:
+        u_4 = float(input("Please enter U: "))
+        v_4 = float(input("Please enter V: "))
+        a_4 = float(input("Please enter A: "))
+
+        TimeFinderOne(u_4, v_4, a_4)
+
+    if answer == 7:
+        v_7 = float(input("Please enter V: "))
+        a_7 = float(input("Please enter A: "))
+        t_7 = float(input("Please enter T: "))
+
+        UniformVelocityFinderOne(v_7, a_7, t_7)
 
 
     if answer == 0:
         sys.exit()
-    else:
-        print("Please enter a right option!")
